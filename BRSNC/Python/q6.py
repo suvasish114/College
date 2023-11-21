ccbcbea7170489d5a491d32ee08059c84c12a03c
Suvasish Das <misuvasish114@gmail.com>
2023-11-21 16:43:40 +0530

    Create q6.py

---
+++ BRSNC/Python/q6.py
@@ -0,0 +1,32 @@
+'''
+INPUT: Two matrices A and B
+OUTPUT: Result of multiplication of those two matrices in C
+DS USED: 2D arrays A[], B[], C[]
+ALGO:
+	START
+		set all elements of C to 0
+		for i in range(len(A))
+			for j in range(len(B[0]))
+				for k in range(len(B))
+					C[i][j]+=A[i][k]*B[k][j]
+		print(C)
+	END
+'''
+
+A = [[12,7,3],
+    [4 ,5,6],
+    [7 ,8,9]]
+B = [[5,8,1,2],
+    [6,7,3,0],
+    [4,5,9,1]]
+result = [[0,0,0,0],
+         [0,0,0,0],
+         [0,0,0,0]]
+
+for i in range(len(A)):
+   for j in range(len(B[0])):
+       for k in range(len(B)):
+           result[i][j] += A[i][k] * B[k][j]
+
+for r in result:
+   print(r)
